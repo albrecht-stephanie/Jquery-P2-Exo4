a8f610e33af691d7dde9ce29758c591e887df201
AS <albrecht.stephanie@outlook.fr>
2019-11-04 22:44:47 +0100

    changer la couleur selon le bouton

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$('#green', '#red', '#blue').on('click', function(){
+    $('#text').css('color', 'green', 'red', 'blue');
+});
+/*$('#red').on('click', function(){
+    $('#text').css('color', 'red');
+});
+$('#blue').on('click', function(){
+    $('#text').css('color', 'blue');
+});
+*/
